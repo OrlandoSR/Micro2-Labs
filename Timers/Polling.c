c735cebd56ae2d5d943e5c31c07ce6be125e81b3
OrlandoSR <42500788+OrlandoSR@users.noreply.github.com>
2022-10-14 16:25:33 -0400

    Update Polling.c

--- Timers/Polling.c
+++ Timers/Polling.c
@@ -9,17 +9,27 @@ int main(void)
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
 	//Setup ports
-	        //Set output pin for buzzer
+	        P1DIR |= BIT4;      //Set output pin for buzzer P1.4
+	        P1OUT &=~ BIT4;     //Clear output
 
 	//Timer setup
-	        //Reset timer in register TBxCTL |= TBCLR;
-	        //Select clock source TBSSEL |= TBSSEL_ACLK;
-	        //Slect Timer/Counter Mode TBxCTL |= MC__CONTINOUS;
+	        TB0CTL |= TBCLR;            //Reset timer in register TBxCTL |= TBCLR;
+	        TB0CTL |= TBSSEL_1;         //Select clock source TBSSEL |= TBSSEL_ACLK;
+	        TB0CTL |= MC__UP;           //Select Timer/Counter in up mode to CCR0
+	        TB0CTL |= CNTL_0;           //Select counter length to 16-bit
+	        TB0CCR0 |= 33;              //Compare register setup
 
 	//Setup TBx overflow IRQ
 	        //Local enable for overflow TB0 |= TBIE;
 	        //Enable maskable IRQs
 	        //Clear IRQ flags TB0CTL |= TBIFG;
+
+	while(1){
+	    if(TB0CCTL0 & CCIFG){
+	        P1OUT ^= BIT4;
+	        TB0CCTL0 &=~ CCIFG;
+	    }
+	}
 
 
 	return 0;
